013dd37829322635c41e836eba2b76ea261412f8
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2024-01-21 18:47:44 +0200

    Add files via upload

---
+++ MyApplication/sprites/GameLoopThread.java
@@ -0,0 +1,46 @@
+package com.sunmeat.thirdproject;
+
+import android.graphics.Canvas;
+
+class GameLoopThread extends Thread {
+    private static final long FPS = 10;
+    private GameView view;
+    private boolean running = false;
+
+    GameLoopThread(GameView view) {
+        this.view = view;
+    }
+
+    void setRunning(boolean run) {
+        running = run;
+    }
+
+    @Override
+    public void run() {
+        long ticksPS = 1000 / FPS;
+        long startTime;
+        long sleepTime;
+        while (running) {
+            Canvas c = null;
+            startTime = System.currentTimeMillis();
+            try {
+                c = view.getHolder().lockCanvas();
+                synchronized (view.getHolder()) {
+                    view.onDraw(c);
+                }
+            } finally {
+                if (c != null) {
+                    view.getHolder().unlockCanvasAndPost(c);
+                }
+            }
+            sleepTime = ticksPS - (System.currentTimeMillis() - startTime);
+            try {
+                if (sleepTime > 0)
+                    sleep(sleepTime);
+                else
+                    sleep(10);
+            } catch (Exception ignored) {
+            }
+        }
+    }
+}
---
+++ MyApplication/sprites/TempSprite.java
@@ -0,0 +1,35 @@
+package com.sunmeat.thirdproject;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import java.util.List;
+
+class TempSprite {
+    private float x;
+    private float y;
+    private Bitmap bmp;
+    private int life = 15;
+    private List<TempSprite> temps;
+
+    TempSprite(List<TempSprite> temps, GameView gameView, float x,
+               float y, Bitmap bmp) {
+        this.x = Math.min(Math.max(x - bmp.getWidth() / 2, 0),
+                gameView.getWidth() - bmp.getWidth());
+        this.y = Math.min(Math.max(y - bmp.getHeight() / 2, 0),
+                gameView.getHeight() - bmp.getHeight());
+        this.bmp = bmp;
+        this.temps = temps;
+    }
+
+    void onDraw(Canvas canvas) {
+        update();
+        canvas.drawBitmap(bmp, x, y, null);
+    }
+
+    private void update() {
+        if (--life < 1) {
+            temps.remove(this);
+        }
+    }
+}
