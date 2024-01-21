913d93077b3a8ca45b35fd679c00365f06ea1004
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2024-01-21 18:48:59 +0200

    Update MainActivity.java

--- MyApplication/sprites/MainActivity.java
+++ MyApplication/sprites/MainActivity.java
@@ -1,7 +1,8 @@
-package com.sunmeat.thirdproject;
+package com.sunmeat.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
