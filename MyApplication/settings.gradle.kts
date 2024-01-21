499a6bb6ad7e12465db1f782218424e53ecc0c55
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2024-01-21 17:54:09 +0200

    Add files via upload

---
+++ MyApplication/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "My Application"
+include(":app")
+ 
