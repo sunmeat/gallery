de6fb461a65843f5ba4fe93121ad87c3069d00a1
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-11-04 11:50:26 +0200

    Refactor MainActivity to new package structure

--- MyApplication/sprites/MainActivity.java
+++ MyApplication/sprites/MainActivity.java
@@ -1,11 +1,9 @@
-package com.sunmeat.myapplication;
+package com.sunmeat.spriteanimation;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
